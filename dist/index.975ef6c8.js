24c93ee5580f58499e1f6c2b8cb7d2c25323cd92
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2022-11-11 13:39:41 +0000

    commit

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -142,7 +142,7 @@
       this[globalName] = mainExports;
     }
   }
-})({"1RB6v":[function(require,module,exports) {
+})({"85bBE":[function(require,module,exports) {
 "use strict";
 var global = arguments[3];
 var HMR_HOST = null;
@@ -538,6 +538,6 @@ var _headerModals = require("./js/header-modals");
     }
 })();
 
-},{}]},["1RB6v","8lqZg"], "8lqZg", "parcelRequired7c6")
+},{}]},["85bBE","8lqZg"], "8lqZg", "parcelRequired7c6")
 
 //# sourceMappingURL=index.975ef6c8.js.map
